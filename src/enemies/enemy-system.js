e69c5679c1c1e1cb23f581629702527cf13f6aa7
aseel salah <aseel.salah90@yahoo.com>
2026-08-26 12:40:20 +0200

    Combat: keep swept-hit bridge during ownership migration

--- src/enemies/enemy-system.js
+++ src/enemies/enemy-system.js
@@ -10,6 +10,9 @@ function installCombatSystem(scene) {
   scene.combatSystem = new CombatSystem(scene);
   scene.enemyCombatSystem = scene.combatSystem.enemy;
   scene.playerDamageSystem = scene.combatSystem.player;
+  scene.onBulletHit = function(bullet, enemy) {
+    return this.combatSystem.hitEnemyByProjectile(bullet, enemy);
+  };
   scene.combatSystem.installOverlaps();
   scene.__enemyCombatFoundationReady = true;
   scene.__playerDamageFoundationReady = true;
