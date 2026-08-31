76bc54ed3560edc9832c6cccdc07d19c9d701422
aseel salah <aseel.salah90@yahoo.com>
2026-08-31 12:57:36 +0200

    Test: isolate Shrapnel from existing forced upgrade offers 1

--- tests/e2e/upgrade-call-rig.spec.ts
+++ tests/e2e/upgrade-call-rig.spec.ts
@@ -24,7 +24,8 @@ test('Call the Rig is offered at level 2 and delegates the summon to the canonic
       'scrap-magnet': 4,
       'armor-plate': 4,
       'piercing-rivets': 3,
-      'ricochet': 2
+      'ricochet': 2,
+      'shrapnel-impact': 2
     });
     scene.level = 2;
     scene.rigSummoned = false;
